03aa93829bf30566c2c685dfe17287fe3cecf9ec
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-11 13:52:19 +0200

    exercise 04 and additional files for the previous exercises

---
+++ week-02/day-01/exercise04/exercise04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Csilla Varga");
+            Console.WriteLine("26");
+            Console.WriteLine("1.75");
+            Console.ReadLine();
+        }
+    }
+}
