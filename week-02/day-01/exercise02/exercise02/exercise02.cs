47061871af1a7df374ee99e40a83d13f32697a68
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-11 13:26:21 +0200

    exercise02 done

---
+++ week-02/day-01/exercise02/exercise02/exercise02.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+            Console.WriteLine("Humpty Dumpty had a great fall.");
+            Console.WriteLine("All the king's horses and all the king's men");
+            Console.WriteLine("Couldn't put Humpty together again.");
+            Console.ReadLine();
+        }
+    }
+}
