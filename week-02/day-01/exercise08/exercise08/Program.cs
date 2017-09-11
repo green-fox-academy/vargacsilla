577dcc4758603ef82633e01ca047fdb65ba78db0
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-11 14:47:50 +0200

    exercise08 done

---
+++ week-02/day-01/exercise08/exercise08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the wariables
+            int a = 123;
+            int b = 526;
+            int tempA = a;
+            a = b;
+            b = tempA;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.ReadLine();
+        }
+    }
+}
