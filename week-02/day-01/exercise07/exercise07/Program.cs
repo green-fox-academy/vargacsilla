d3b173ac3aa18b6634d1d26b1c3a1eec938b6994
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-11 14:34:46 +0200

    exercise07 done

---
+++ week-02/day-01/exercise07/exercise07/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            int favNum = 2;
+            // And print it like this: "My favorite number is: 8"
+            Console.WriteLine("My favourite number is " + favNum + ".");
+            Console.ReadLine();
+        }
+    }
+}
