439658a2333f56978a7118594393ab993a4a7ba0
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-11 13:21:38 +0200

    exercise01 done

---
+++ week-02/day-01/exercise01/exercise01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Csicsi!");
+            Console.ReadLine();
+            
+        }
+    }
+}
