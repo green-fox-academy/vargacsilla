ebfbac58623fcfc85b1f2f9b5e5f90db2f691c2e
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 16:11:48 +0200

    AppendA done

---
+++ week-02/day-02/AppendA/AppendA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            Console.WriteLine(AppendA(am));
+            Console.ReadLine();
+        }
+
+        private static string AppendA(string text)
+        {
+            return text += "a";
+        }
+    }
+}
