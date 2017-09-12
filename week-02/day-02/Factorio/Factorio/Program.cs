374825a4a0c5ae41ee105163e2794c7907d2d8cd
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 16:55:32 +0200

    Factorio done

---
+++ week-02/day-02/Factorio/Factorio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorio(5));
+            Console.ReadLine();
+        }
+
+        public static int Factorio(int num)
+        {
+            for (int i = (num - 1); i >= 1 ; i--)
+            {
+                num *= i;
+            }
+            return num;
+        }
+    }
+}
