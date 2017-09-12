631f068bce08172f160110bf8d39024583037ec4
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 16:05:15 +0200

    greet done

---
+++ week-02/day-02/greet/greet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            greet(al);
+            Console.ReadLine();
+        }
+
+        private static void greet(string name)
+        {
+            Console.WriteLine("Greetings, dear {0}!", name);
+        }
+    }
+}
