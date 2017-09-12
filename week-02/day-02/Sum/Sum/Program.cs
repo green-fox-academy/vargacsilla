5f021a4608ec9497fc275a515dc588c141595c0a
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 16:39:50 +0200

    Sum done - there might be a more simple solution

---
+++ week-02/day-02/Sum/Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(5));
+            Console.ReadLine();
+        }
+
+        private static int Sum(int number)
+        {
+            int numSum = 0; 
+            for (int i = 1; i <= number; i++)
+            {
+                numSum += i;
+            }
+            number = numSum;
+            return number;
+        }
+    }
+}
