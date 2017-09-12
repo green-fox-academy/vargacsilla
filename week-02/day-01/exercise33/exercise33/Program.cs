0b0fa616b31a52d147ed6bb97901b96daf8bede6
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 13:54:26 +0200

    exercise33 done

---
+++ week-02/day-01/exercise33/exercise33/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercise33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 100;
+            int userNum;
+            Console.WriteLine("Guess a number!");
+
+            do
+            {
+                userNum = int.Parse(Console.ReadLine());
+                if (userNum < myNum)
+                {
+                    Console.WriteLine("The stored number is higher.");
+                }
+                else if (userNum > myNum)
+                {
+                    Console.WriteLine("The stored number is lower.");
+                }
+                else
+                {
+                Console.WriteLine("You found the number! It's {0}.", myNum);
+                }
+            }
+            while (userNum != myNum);
+            Console.ReadLine();
+        }
+    }
+}
