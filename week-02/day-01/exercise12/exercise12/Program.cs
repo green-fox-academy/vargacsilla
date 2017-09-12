bc85f89325f7eefbd6dd642463cf3ea885fdf94a
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 23:17:37 +0200

    exercise12 done

---
+++ week-02/day-01/exercise12/exercise12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = 10.1;
+            double height = 20.2;
+            double length = 50.3;
+
+            Console.WriteLine("Surface Area: " + calculateSurface(width, height, length));
+            Console.WriteLine("Volume: " + calculateVolume(width, height, length));
+
+            Console.ReadLine();
+        }
+
+        public static double calculateVolume (double width, double height, double length)
+        {
+            double volume = width * height * length;
+            return volume;
+        }
+
+        public static double calculateSurface(double width, double height, double length)
+        {
+            double surface = 2 * ((width * height) + (height * length) + (length * width));
+            return surface;
+
+        }
+    }
+}
