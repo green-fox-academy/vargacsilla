8777d59ab034ca6b949f93b6a732b3a2c6c362d6
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 23:39:33 +0200

    exercise14 done

---
+++ week-02/day-01/exercise14/exercise14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Hello, " + Console.ReadLine() + "!");
+            Console.ReadLine();
+        }
+    }
+}
