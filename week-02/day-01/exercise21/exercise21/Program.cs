59926c6103d31b275c9b2053bb28b4756468791b
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 13:06:07 +0200

    exercise21 done

---
+++ week-02/day-01/exercise21/exercise21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBoys;
+            int numGirls;
+
+            Console.WriteLine("How many girls and boys came to the party? Type a number and press Enter.");
+            numGirls = int.Parse(Console.ReadLine());
+            numBoys = int.Parse(Console.ReadLine());
+
+            int numAll = numGirls + numBoys;
+
+            if (numAll > 20 && numGirls == numBoys)
+            {
+                Console.WriteLine("The party is exellent!");
+            }
+            else if (numAll > 20)
+            {
+                Console.WriteLine("Quite cool party!");
+            }
+            else if (numAll < 20)
+            {
+                Console.WriteLine("Average party...");
+            }
+            else if (numGirls == 0)
+            {
+                Console.WriteLine("Sausage party...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
