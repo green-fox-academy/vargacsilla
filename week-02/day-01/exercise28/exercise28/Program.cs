8ec4dce02c93937a6423b2f022ff4454555b09c1
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 13:31:55 +0200

    exercise28 done

---
+++ week-02/day-01/exercise28/exercise28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("I'll draw you a triangle, if you tell me how many lines tall you want it to be. Give me a number!");
+            int triangleLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= triangleLines; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
