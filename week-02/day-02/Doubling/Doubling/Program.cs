3adf87eb8dd5c66c5a01d2667bf060a5ee3a1ade
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 14:01:33 +0200

    Doubling done

---
+++ week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+
+        public static int Doubling (int num)
+        {
+            num *= 2;
+            return num;
+        }
+    }
+}
