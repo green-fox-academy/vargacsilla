795dfa1878d06096ab9e6a81b28d1e916af9dfd0
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-12 17:17:02 +0200

    Printer done

---
+++ week-02/day-02/Printer/Printer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer("valami", "masvalami", "barmi", "vegtelen", "szavak", "huhuuu");
+			Console.ReadLine();
+        }
+
+        public static void Printer(params string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.Write("{0}, ", word);
+            }
+        }
+    }
+}
