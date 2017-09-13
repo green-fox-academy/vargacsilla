8d1e7c88633b0bb659000eb6b5966811412a6a6a
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 09:14:01 +0200

    Third done

---
+++ week-02/day-02/Third/Third/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = new int[4] { 4, 5, 6, 7 };
+            Console.WriteLine(q[2]);
+            Console.ReadLine();
+        }
+    }
+}
