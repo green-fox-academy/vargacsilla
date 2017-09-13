d4dc968b05dd7e208a3ebcbc7589883dfe99ae68
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 23:29:12 +0200

    exercise18 done

---
+++ week-02/day-01/exercise18/exercise18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, give me a number!");
+            int userNum;
+
+            while (!(int.TryParse(Console.ReadLine(), out userNum)))
+            {
+                Console.WriteLine("That's not an integer number, please try again!");
+            }   
+
+            if (userNum % 2 == 0)
+                Console.WriteLine("That's an even number.");
+            else
+                Console.WriteLine("That's an odd number.");
+
+            Console.ReadLine();
+        }
+    }
+}
