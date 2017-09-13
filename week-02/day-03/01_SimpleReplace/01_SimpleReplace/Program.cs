62a8469f2e4434ca5a351a0b16577d78f857d2e0
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 11:45:29 +0200

    01_SimpleReplace done

---
+++ week-02/day-03/01_SimpleReplace/01_SimpleReplace/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01_SimpleReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+            example = example.Replace("dishwasher", "galaxy");
+            Console.WriteLine(example);
+            Console.ReadLine();
+        }
+    }
+}
