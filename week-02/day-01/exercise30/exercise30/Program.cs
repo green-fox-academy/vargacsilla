5454ef4c8658d90040800aa8e282afee20c7afbf
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 08:17:22 +0200

    exercise30 done

---
+++ week-02/day-01/exercise30/exercise30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pyramidRows = 10;
+            string pyr = "*";
+            int spaceNum = pyramidRows - 1;
+
+            for (int i = 0; i < pyramidRows; i++)
+            {
+                for (int j = spaceNum; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                spaceNum -= 1;
+                Console.WriteLine(pyr);
+                pyr += "**";
+            }
+            Console.ReadLine();
+        }
+    }
+}
