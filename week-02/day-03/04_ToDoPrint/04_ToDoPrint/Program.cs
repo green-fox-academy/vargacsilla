11845d94141fd54209d7fd7c3c39b80cc8aabf83
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 14:48:50 +0200

    04_ToDoPrint done

---
+++ week-02/day-03/04_ToDoPrint/04_ToDoPrint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _04_ToDoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoList = "- Buy milk\n";
+            int lastIndex = todoList.Length;
+            todoList = todoList.Insert(0, "My todo:\n");
+            todoList = todoList.Insert(todoList.Length, "- Download games\n");
+            todoList = todoList.Insert(todoList.Length, "\t- Diablo");
+
+            //string todoList = "- Buy milk\n";
+            //todoList = todoList.Insert(0, "My todo:\n");
+            //StringBuilder todoListBuild = new StringBuilder(todoList);
+            //todoList.Insert(0, "My todo:\n");
+            //todoListBuild.Append("- Download games\n").Append("\t").Append("- Diablo");
+            //todoList = todoListBuild;
+
+            Console.WriteLine(todoList);
+            Console.ReadLine();
+        }
+    }
+}
