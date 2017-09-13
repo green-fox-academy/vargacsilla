7e27d9c00a48da68339d7a85f2c5baeb7c6d03f7
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 07:40:52 +0200

    exercise17 done - needs a simpler solution

---
+++ week-02/day-01/exercise17/exercise17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise17
+{
+    class Program
+    {
+        private static int first;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me 5 numbers (after each number press Enter)!");
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+            int fifth = int.Parse(Console.ReadLine());
+
+            int sum = first + second + third + fourth + fifth;
+            int average = sum / 5;
+
+            Console.WriteLine("Sum: {0}, Average: {1}", sum, average);
+            Console.ReadLine();
+        }
+    }
+}
