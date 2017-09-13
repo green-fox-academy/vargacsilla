46b0cea9e8dbe4378aaf0f8e00b5c3edf80c6f1e
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 22:55:11 +0200

    exercise16 done

---
+++ week-02/day-01/exercise16/exercise16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+            Console.WriteLine("How many chickens are there on the farm?");
+            int chickens = int.Parse(Console.ReadLine());
+            Console.WriteLine("And how many pigs?");
+            int pigs = int.Parse(Console.ReadLine());
+            Console.WriteLine("The chickens and the pigs together have {0} legs.", (chickens * 2) + (pigs * 4));
+            Console.ReadLine();
+        }
+    }
+}
