e374bc0f1fd24c1482ecbc8c613df68bb3df4287
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 22:47:34 +0200

    exercise15 done

---
+++ week-02/day-01/exercise15/exercise15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+            Console.WriteLine("Distance in kilometers (give me an integer): ");
+            int kms = int.Parse(Console.ReadLine());
+            double milePerKm = 0.621371192;
+            Console.WriteLine("= {0} miles.", kms * milePerKm);
+            Console.ReadLine();
+        }
+    }
+}
