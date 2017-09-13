1dfc51f804f48a88d9d76b317f1f91b1607dd217
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 12:52:28 +0200

    02_UrlFixer done

---
+++ week-02/day-03/02_UrlFixer/02_UrlFixer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02_UrlFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            // url = url.Replace("bots", "odds");
+            // url = url.Insert(5, ":");
+
+            // url = url.Replace("bots", "odds");
+            // url = url.Replace("https", "https:");
+
+            url = url.Replace("bots", "odds").Insert(5, ":");
+
+            Console.WriteLine(url);
+            Console.ReadLine();
+        }
+    }
+}
