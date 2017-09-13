da166e72fb3e40c38c6bf3223bb6ab906f064103
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 22:29:57 +0200

    05_ElementFinder done

---
+++ week-02/day-03/05_ElementFinder/05_ElementFinder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_ElementFinder
+{
+    public class ElementFinder
+    {
+        public static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            //// Listname[indexNum].Equals(value)
+
+            //bool contains = false;
+            //for (int i = 0; i < List.Count; i++)
+            //{
+            //    if (List[i].Equals(7))
+            //    {
+            //        contains = true;
+            //    }
+            //}
+            //if (contains == true)
+            //{
+            //Console.WriteLine("Hoorray, there's a 7 in the list!");
+            //}
+            //else
+            //{
+            //Console.WriteLine("Nooo, no 7 in the list!");
+            //}
+
+            // listname.Contains(itemToLookFor)
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray, there's a 7 in the list!");
+            }
+            else
+            {
+                Console.WriteLine("Nooo, no 7 in the list!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
