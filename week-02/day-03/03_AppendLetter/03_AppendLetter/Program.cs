d8737a5e291bf2d8b14ceea788d4926ac1cfc069
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 16:43:12 +0200

    03_AppendLetter done

---
+++ week-02/day-03/03_AppendLetter/03_AppendLetter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_AppendLetter
+{
+    public class AppendLetter
+    {
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+                Console.Write(far[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
