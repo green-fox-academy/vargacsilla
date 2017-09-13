e9fcca2eb627d9df42d86cc3bce10b6e0ca2bc47
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 10:52:39 +0200

    SumElements done

---
+++ week-02/day-02/SumElements/SumElements/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = new int[4] { 54, 23, 66, 12 };
+            Console.WriteLine(r[1] + r[2]);
+            Console.ReadLine();
+        }
+    }
+}
