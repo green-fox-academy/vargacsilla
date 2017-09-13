3064424cebf0ac7d1d2c283dcbd363527490711e
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 15:57:23 +0200

    01_Solar done

---
+++ week-02/day-03/01_Solar/01_Solar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Solar
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+            planetList.Insert(5, "Saturn");
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            } 
+
+            Console.ReadLine();
+        }
+    }
+}
