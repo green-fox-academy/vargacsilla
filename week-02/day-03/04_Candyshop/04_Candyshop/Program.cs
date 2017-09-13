00e7f04a10cbaee4f1bd4a02b0658a967f8d2f2e
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-13 17:07:34 +0200

    04_Candyshop done - not really sophisticated solution

---
+++ week-02/day-03/04_Candyshop/04_Candyshop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_CandyShop
+{
+    public class Candyshop
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list[1] = "Croissant";
+            list[3] = "Ice Cream";
+
+            foreach (var food in list)
+            {
+                Console.Write("{0}, ", food);
+            }
+            Console.ReadLine();
+        }
+    }
+}
