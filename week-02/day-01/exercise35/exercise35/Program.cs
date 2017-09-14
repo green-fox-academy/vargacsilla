d8f57218ed9a97f4c8a3b4def898e2153e63603d
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 16:47:54 +0200

    exercise35 done

---
+++ week-02/day-01/exercise35/exercise35/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercise35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 5;
+
+
+            for (int i = 0; i < width; i++)
+            {
+                int startNum;
+
+                if (i % 2 == 0)
+                    startNum = 1;
+                else
+                    startNum = 0;
+                
+                for (int j = startNum; j < width; j++)
+                {
+                    if (j % 2 == 0)
+                        Console.Write(" ");
+                    else
+                        Console.Write("%");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
