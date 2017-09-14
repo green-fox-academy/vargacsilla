4c44ea9a106a3b648265bdb3361406cda42de2cc
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 09:56:36 +0200

    exercise25 done

---
+++ week-02/day-01/exercise25/exercise25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercise25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for a number and prints the multiplication table with that number
+            //
+            // Example:
+            // The number 15 should print:
+            //
+            // 1 * 15 = 15
+            // 2 * 15 = 30
+            // 3 * 15 = 45
+            // 4 * 15 = 60
+            // 5 * 15 = 75
+            // 6 * 15 = 90
+            // 7 * 15 = 105
+            // 8 * 15 = 120
+            // 9 * 15 = 135
+            // 10 * 15 = 150
+            Console.WriteLine("Give me a number!");
+            int userNum = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", i, userNum, i * userNum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
