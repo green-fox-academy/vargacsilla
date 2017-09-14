843ceb48ed609448579b855a4dcaf6fbcb695d88
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 08:00:46 +0200

    exercise20 done

---
+++ week-02/day-01/exercise20/exercise20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me two numbers!");
+            string input = Console.ReadLine();
+            string[] userNums = input.Split(' ');
+            long first = long.Parse(userNums[0]);
+            long second = long.Parse(userNums[1]);
+
+            if (first > second)
+                Console.WriteLine("The bigger number is {0}.", first);
+            if (first == second)
+                Console.WriteLine("These numbers are equal.");
+            else
+                Console.WriteLine("The bigger number is {0}.", second);
+
+            Console.ReadLine();
+        }
+    }
+}
