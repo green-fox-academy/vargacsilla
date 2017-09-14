64de9d7b13620c7423ce0bde6ef11a28e2efbc27
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 11:35:36 +0200

    exercise 29 - code cleaned

--- week-02/day-01/exercise29/exercise29/Program.cs
+++ week-02/day-01/exercise29/exercise29/Program.cs
@@ -6,16 +6,6 @@ namespace exercise29
     {
         static void Main(string[] args)
         {
-            // Write a program that reads a number from the standard input, then draws a
-            // pyramid like this:
-            //
-            //
-            //    *
-            //   ***
-            //  *****
-            // *******
-            //
-            // The pyramid should have as many lines as the number was
             Console.WriteLine("Give me a number and I'll draw a pyramid with that many lines!");
             int rowsNum = int.Parse(Console.ReadLine());
 
@@ -30,7 +20,6 @@ namespace exercise29
             Console.WriteLine(pyramidBody);
             pyramidBody += "**";
             }
-                
             Console.ReadLine();
         }
     }
