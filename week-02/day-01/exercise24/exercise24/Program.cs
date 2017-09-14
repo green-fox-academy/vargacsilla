1a7d75b3dcc34545e2b090aeaf8a1c3edb07c79a
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 09:50:54 +0200

    exercise24 done

---
+++ week-02/day-01/exercise24/exercise24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercise24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 500; )
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
