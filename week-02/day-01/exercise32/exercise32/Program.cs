25fb47fc0abea904ff213f1b7619db738ebd1299
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 16:05:35 +0200

    exercise32 done

---
+++ week-02/day-01/exercise32/exercise32/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exercise32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Square side:");
+            int squareSide = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < squareSide; i++) // Első sor
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < squareSide - 2; i++) // minden közbenső sor
+            {
+                Console.Write("%");
+
+                for (int j = 0; j < squareSide - 2; j++)
+                {
+                    if (j == i)
+                        Console.Write("%");
+                    else
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("%");
+            }
+
+            for (int i = 0; i < squareSide; i++) // Utolsó sor
+            {
+                Console.Write("%");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
