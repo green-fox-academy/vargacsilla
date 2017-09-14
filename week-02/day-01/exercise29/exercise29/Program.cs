d222e83632a7edd6790292dad8b4a78e1386bb68
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 11:25:24 +0200

    exercise27 done

---
+++ week-02/day-01/exercise29/exercise29/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercise29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+            Console.WriteLine("Give me a number and I'll draw a pyramid with that many lines!");
+            int rowsNum = int.Parse(Console.ReadLine());
+
+            string pyramidBody = "*";
+            char pyramidVoid = ' ';
+
+            for (int i = rowsNum; i > 0; i--)
+            {
+                for (int j = i - 1; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+            Console.WriteLine(pyramidBody);
+            pyramidBody += "**";
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
