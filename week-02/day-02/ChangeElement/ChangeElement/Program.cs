423563126d48d23a1fe58d6faee06e04a3d9bc0a
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 17:53:50 +0200

    ChangeElement done

---
+++ week-02/day-02/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            // s[3] = 4;
+            s.SetValue(4, 3);
+
+            foreach (var item in s)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine("\n" + s[3]);
+            Console.ReadLine();
+        }
+    }
+}
