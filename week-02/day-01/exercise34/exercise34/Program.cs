af229e42683fa8a3106836c79098f3afae5b8ed8
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-14 16:23:11 +0200

    exercise34 done

---
+++ week-02/day-01/exercise34/exercise34/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercise34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            Console.WriteLine("Give me a number! (Not too big, if possible...)");
+            int numOfNums = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thanks! Now give {0} different numbers!", numOfNums);
+            int sumOfNums = 0;
+
+            for (int i = 0; i < numOfNums; i++)
+            {
+                sumOfNums += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Sum: {0}, Average {1}", sumOfNums, sumOfNums / numOfNums);
+            Console.ReadLine();
+        }
+    }
+}
