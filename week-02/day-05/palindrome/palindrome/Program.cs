233319a961946d8dee7c8ea417c40982874d843b
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-15 10:25:21 +0200

    palindrome done

---
+++ week-02/day-05/palindrome/palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a word!");
+            string wordInput = Console.ReadLine();
+            Console.WriteLine(Palindrome(wordInput));
+            Console.ReadLine();
+        }
+
+        public static string Palindrome(string word)
+        {
+            string palindromeWord = word;
+            for (int i = 0; i < word.Length; i++)
+            {
+                palindromeWord += word[word.Length - i - 1];
+            }
+            return palindromeWord;
+        }
+    }
+}
