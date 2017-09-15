f4e0bae525545cb9ff7b25bc6def8122ace83d36
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-15 11:55:31 +0200

    Armstrong number - structure and function: get number

---
+++ week-02/day-05/Armstrong_number/Armstrong_number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Armstrong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get number
+            int userNum = GetNumber();
+            // check if its armstrong
+            bool isArmstrong = IsItArmstrongNumber(userNum);
+            //// split number to digits
+            //// raise every digit to the nth power
+            //// add numbers together
+
+            Console.WriteLine(isArmstrong);
+            Console.ReadLine();
+        }
+
+        public static int GetNumber()
+        {
+            int getNumber = int.Parse(Console.ReadLine());
+            return getNumber;
+        }
+    }
+}
