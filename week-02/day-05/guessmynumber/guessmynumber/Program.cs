16fa2c39c80d9da2066258c108d030aeec205234
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-15 10:41:46 +0200

    Guess my number - get range, random number generated

---
+++ week-02/day-05/guessmynumber/guessmynumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace guessmynumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for range
+            //get range
+            Console.WriteLine("Lets play a guessing game! How big should the range be?");
+            int range = int.Parse(Console.ReadLine());
+            //generate a random number between 0 and range
+            Random rnd = new Random();
+            int theNumber = rnd.Next(range);
+
+            Console.WriteLine(theNumber);
+            Console.ReadLine();
+
+        }
+    }
+}
