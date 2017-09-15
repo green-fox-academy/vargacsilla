e46d175ac7c1b75eb3a85206cc3d2db2d5ed33d5
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-15 12:51:23 +0200

    Armstrong number - function: SplitToDigits

--- week-02/day-05/Armstrong_number/Armstrong_number/Program.cs
+++ week-02/day-05/Armstrong_number/Armstrong_number/Program.cs
@@ -19,6 +19,33 @@ namespace Armstrong_number
             Console.ReadLine();
         }
 
+        private static bool IsItArmstrongNumber(int inputNum)
+        {
+            bool isArmstrong;
+            List<int> digitsList = new List<int>();
+
+            digitsList = SplitToDigits(inputNum);
+
+
+        
+            return isArmstrong;
+        }
+
+        private static List<int> SplitToDigits(int inputNum)
+        {
+            List<int> digits = new List<int>();
+
+            for (int i = 0; inputNum >= 0; i++)
+            {
+                digits[i] = inputNum % 10;
+                inputNum = inputNum / 10;
+            };
+
+            digits.Reverse();
+
+            return digits;
+        }
+
         public static int GetNumber()
         {
             int getNumber = int.Parse(Console.ReadLine());
