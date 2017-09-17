6a747589659db8228f09c7ab40733d3304fecaf6
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-17 17:46:11 +0200

    SumAll done

---
+++ week-02/day-02/SumAll/SumAll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            int[] ai = new int[] { 3, 4, 5, 6, 7 };
+            int sumAll = 0;
+            foreach (var item in ai)
+            {
+                sumAll += item;
+            }
+            Console.WriteLine(sumAll);
+            Console.ReadLine();
+        }
+    }
+}
