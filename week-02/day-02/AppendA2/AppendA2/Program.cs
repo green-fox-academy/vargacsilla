e8828dce5a832a19c5039954b9fb3bd164958eca
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-17 16:54:18 +0200

    AppendA2 done

---
+++ week-02/day-02/AppendA2/AppendA2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AppendA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            string[] nimals = new string[] { "kuty", "macsk", "cic" };
+
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                nimals[i] += "a";
+                Console.WriteLine(nimals[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
