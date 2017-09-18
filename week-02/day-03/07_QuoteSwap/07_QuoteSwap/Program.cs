2b7da1c5acb2950cfc97f22d2c2623b69e7e8113
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-18 07:15:36 +0200

    07 QuoteSwap done

---
+++ week-02/day-03/07_QuoteSwap/07_QuoteSwap/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07_QuoteSwap
+{
+    public class QuoteSwap
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            Swap(list, 2, 5);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+        public static void Swap (List<string> list, int index1, int index2)
+        {
+            string temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+        
+    }
+}
