9cab5497a68f00078c99ba815515e0c746cf8fc4
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-19 17:06:37 +0200

    add CopyTo function

---
+++ week-03/day-02/06_CopyFile/06_CopyFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _06_CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyTo(@"./../../testOriginal.txt", @"./../../testCopy.txt");
+            Console.Read();
+        }
+
+        private static void CopyTo(string originalFile, string copyFile)
+        {
+
+            bool success = true;
+            try
+            {
+                    string[] content = File.ReadAllLines(originalFile);
+                    File.WriteAllLines(copyFile, content);
+            }
+            catch
+            {
+                success = false;
+            }
+            finally
+            {
+                Console.WriteLine(success);
+            }
+        }
+    }
+}
