ae79b61d8a19d2d290f532026248b50e48b529ef
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-19 11:13:08 +0200

    DivideByZero done

---
+++ week-03/day-02/01_DivideByZero/01_DivideByZero/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideTen(GetInputNum());
+            Console.Read();
+        }
+
+        private static void DivideTen(int userNum)
+        {
+            try
+            {
+                int result = 10 / userNum;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("fail");
+            }
+
+        }
+
+        private static int GetInputNum()
+        {
+            int userInput = int.Parse(Console.ReadLine());
+            return userInput;
+        }
+    }
+}
