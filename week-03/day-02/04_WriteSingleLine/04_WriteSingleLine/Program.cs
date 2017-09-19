4cc15d2d5965539c17e547c787854e946cd8137c
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-19 14:11:47 +0200

    04_WriteSingleLine done

---
+++ week-03/day-02/04_WriteSingleLine/04_WriteSingleLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _04_WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "./../../my-file.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("Varga Csilla");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+        }
+    }
+}
+
