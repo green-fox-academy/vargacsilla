3b550b19f083a29cafc8e9c33906d95e6aa00493
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-19 17:47:49 +0200

    add DeDuplicate function

---
+++ week-03/day-02/07_Doubled/07_Doubled/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _07_Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"./../../../../duplicated-chars.txt";
+            string newPath = @"./../../../../deduplicated-chars.txt";
+            // To modify original file:
+            // DeDuplicate(path);
+            DeDuplicate(path, newPath);
+        }
+
+        // To modify original file:
+        // private static void DeDuplicate(string path)
+        private static void DeDuplicate(string path, string newPath)
+        {
+            string[] lines = File.ReadAllLines(path);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j ++)
+                {
+                    lines[i] = lines[i].Remove(j, 1);
+                }
+                File.WriteAllLines(newPath, lines);
+            }
+        }
+    }
+}
