3d56a6ccfb96f9f8cb6f62c114c2e3ab61f1c550
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-19 13:19:15 +0200

    03_CountLines done

---
+++ week-03/day-02/03_CountLines/03_CountLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _03_CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfLines(@"C:\Users\Csilla\greenfox\vargacsilla\week-03\day-02\02_PrintEachLine\02_PrintEachLine\Program.cs"));
+            Console.Read();
+        }
+
+        private static int NumberOfLines(string path)
+        {
+            int numberOfLines = 0;
+            try
+            {
+                string[] allLines = File.ReadAllLines(path);
+                numberOfLines = allLines.Length;
+            }
+            catch
+            {
+                numberOfLines = 0;
+            }
+            return numberOfLines;
+        }
+    }
+}
