e8891b925a1263e1a67547a23d6bb51330180223
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-20 12:57:07 +0200

    Diagonals done

---
+++ week-03/day-03/03_Diagonals/03_Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _03_Diagonals
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Green);
+
+            foxDraw.DrawLine(0, 0, canvas.Width, canvas.Height);
+            foxDraw.DrawLine(0, canvas.Height, canvas.Width, 0);
+        }
+    }
+}
