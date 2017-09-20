7797d0c9478d6d8fdc5a0ecebac6dc9e7667d892
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-20 14:29:43 +0200

    08_PositionSquare done

---
+++ week-03/day-03/08_PositionSquare/08_PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _08_PositionSquare
+{
+
+    public partial class MainWindow : Window
+    {
+        public static Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            int squareNum = 3;
+            for (int i = 0; i < squareNum; i++)
+            {
+            DrawFixedSizeRect(rnd.Next(250), rnd.Next(250));
+            }
+        }
+
+        private void DrawFixedSizeRect(double x, double y)
+        {
+            double a = 50;
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawRectangle(x, y, a, a);
+        }
+    }
+}
