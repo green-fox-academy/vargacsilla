9c606f83c7daaa8e0a2249c1890e16a5078016ee
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-20 16:55:15 +0200

    13_Checkerboard done

---
+++ week-03/day-03/13_Checkerboard/13_Checkerboard/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace _13_Checkerboard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            double x = 0;
+            double y = 0;
+            double a = canvas.Width / 10;
+
+            for (int i = 0; y < canvas.Height; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    x += a;
+                }
+                
+                for (int j = 0; x < canvas.Width; j++)
+                {
+                    foxDraw.FillColor(Colors.Black);
+                    foxDraw.DrawRectangle(x, y, a, a);
+                    x += 2 * a;
+                }
+                x = 0;
+                y += a;
+            }
+        }
+    }
+}
