78dec2ae4a2ec6556c1b6df66110be0cade243c9
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-20 17:48:12 +0200

    14_EveryThingGoesToCenter done with two separate for cycles

---
+++ week-03/day-03/14_EveryThingGoesToCenter/14_EveryThingGoesToCenter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _14_EveryThingGoesToCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            double x = 0;
+            double y = 0;
+
+            for (; y <= canvas.Width; y += 20)
+            {
+                LineToCenter(x, y);
+                LineToCenter(canvas.Width, y);
+            }
+
+            for (; x < canvas.Height; x += 20)
+            {
+                LineToCenter(x, canvas.Height);
+                LineToCenter(x, 0);
+            }
+            // Two lines are at the wrong place, but otherwise it's working lol:
+            //
+            // for (; x <= canvas.Width; )
+            // {
+            //    for (; y < canvas.Height; y += 20)
+            //    {
+            //        LineToCenter(x, y);
+            //        LineToCenter(canvas.Width, y);
+            //    }
+            //    x += 20;
+            //    LineToCenter(x, canvas.Height);
+            //    LineToCenter(x, 0);
+            //}
+        }
+
+        private void LineToCenter(double x, double y)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            var startPoint = new Point(x, y);
+            var center = new Point(canvas.Width / 2, canvas.Height / 2);
+            foxDraw.DrawLine(startPoint, center);
+        }
+    }
+}
