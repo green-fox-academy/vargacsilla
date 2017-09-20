65c411198a908a9f41ce312c69f6a29899b68002
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-20 13:34:34 +0200

    06_SquareInTheCenter done

---
+++ week-03/day-03/06_SquareInTheCenter/06_SquareInTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _06_SquareInTheCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.DarkSeaGreen);
+            foxDraw.FillColor(Colors.DarkSeaGreen);
+            double a = 10;
+            foxDraw.DrawRectangle((canvas.Width / 2) - (a / 2), (canvas.Height / 2) - (a / 2), a, a);
+        }
+    }
+}
