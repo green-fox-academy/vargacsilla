ef381b753484e8af0c11e913ce3e6c3b5f4e2c0b
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 16:09:59 +0200

    09_StringsAgainAgain done

---
+++ week-03/day-04/09_StringsAgainAgain/09_StringsAgainAgain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_StringsAgainAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+            string s = "AXxBXxCXx";
+            Console.WriteLine(SwapXToY(s));
+            Console.ReadLine();
+        }
+
+        private static string SwapXToY(string s)
+        {
+            if (s.Length == 1)
+                return s;
+            else
+                return s = s[0] + "*" + SwapXToY(s.Substring(1));
+
+        }
+    }
+}
