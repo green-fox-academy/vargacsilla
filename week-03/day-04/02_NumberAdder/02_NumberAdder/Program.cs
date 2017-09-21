ec5f715b7c65ab07902d0163c28457d17d409e50
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 10:15:51 +0200

    02_NumberAdder done

---
+++ week-03/day-04/02_NumberAdder/02_NumberAdder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The result is:" + NumberAdder(10));
+            Console.ReadLine();
+        }
+
+        private static int NumberAdder(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n += NumberAdder(n - 1);
+        }
+    }
+}
