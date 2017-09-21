edac60bcab6f0c72b7a478eb7b73c3792249ce14
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 15:46:41 +0200

    07_Strings done

---
+++ week-03/day-04/07_Strings/07_Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            string s = "AXxBXxCXx";
+            Console.WriteLine(SwapXToY(s));
+            Console.ReadLine();
+        }
+
+        private static string SwapXToY(string s)
+        {
+            if (s.Length == 0)
+                return s;
+            else if (s[0] == 'x')
+            {
+                return s = "y" + SwapXToY(s.Substring(1));
+            }
+            else
+                return s = s[0] + SwapXToY(s.Substring(1));
+
+        }
+    }
+}
