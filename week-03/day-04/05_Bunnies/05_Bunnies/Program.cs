84613647a1dd8dc4df4af3141dc63369c1f5fff7
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 11:53:01 +0200

    05_Bunnies done

---
+++ week-03/day-04/05_Bunnies/05_Bunnies/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountEars(23));
+            Console.ReadLine();
+        }
+
+        private static int CountEars(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+                return 2 + CountEars(n - 1);
+        }
+    }
+}
