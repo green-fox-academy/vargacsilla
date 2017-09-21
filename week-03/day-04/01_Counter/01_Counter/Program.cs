fae8cac52a5b1309b044a0f8727782b5e1e5ffc4
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 10:05:54 +0200

    01_Counter done

---
+++ week-03/day-04/01_Counter/01_Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(40);
+            Console.ReadLine();
+        }
+
+        private static int CountDown(int n)
+        {
+            if (n == 0)
+                return n = 1;
+            else
+                Console.WriteLine(n);
+                return CountDown(n - 1);
+        }
+    }
+}
