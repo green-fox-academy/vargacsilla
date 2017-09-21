ac0bf9cea1dac0e845d96a66b419de55d61acfb4
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-21 11:46:04 +0200

    04_Power done

---
+++ week-03/day-04/04_Power/04_Power/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToThePower(2, 10));
+            Console.Read();
+        }
+
+        private static int ToThePower(int n, int p)
+        {
+            if (p == 0)
+                return 1;
+            else
+                p--;
+            return n * ToThePower(n, p);
+        }
+    }
+}
