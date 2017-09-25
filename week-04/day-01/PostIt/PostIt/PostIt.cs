264910e672cb49cff8d6b29e8e468c3cbda082d4
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-25 13:17:43 +0200

    PostIt done

---
+++ week-04/day-01/PostIt/PostIt/PostIt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace PostIt
+{
+    public struct PostIt
+    {
+        public string BackgroundColor;
+        public string Text;
+        public string TextColor;
+    }
+}
---
+++ week-04/day-01/PostIt/PostIt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postIt1;
+            postIt1.BackgroundColor = "orange";
+            postIt1.TextColor = "blue";
+            postIt1.Text = "Idea 1";
+
+            PostIt postIt2;
+            postIt2.BackgroundColor = "pink";
+            postIt2.TextColor = "black";
+            postIt2.Text = "Awsome";
+
+            PostIt postIt3;
+            postIt3.BackgroundColor = "yellow";
+            postIt3.TextColor = "green";
+            postIt3.Text = "Superb!";
+        }
+    }
+}
