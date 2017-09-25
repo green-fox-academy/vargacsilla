cd987a4d732db0e69f977d46042b3e1553d88982
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-25 16:46:30 +0200

    Domino done

---
+++ week-04/day-01/Domino/Domino/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domino
+{
+    public class Dominoes
+    {
+        public static void Main(string[] args)
+        {
+            var dominoes = InitializeDominoes();
+
+            List<int[]> sortedDominoes = new List<int[]>();
+            sortedDominoes = SortDominoes(dominoes);
+
+            foreach (var domino in sortedDominoes)
+            {
+                Console.Write("[" + domino[0] + " " + domino[1] + "] ");
+            }
+           
+            Console.ReadLine();
+
+        }
+
+        private static List<int[]> SortDominoes(List<Domino> dominoes)
+        {
+            List<int[]> sortedDominoes = new List<int[]> { };
+            sortedDominoes.Add(dominoes[0].GetValues());
+            dominoes.RemoveAt(0);
+
+            for (int i = 0; dominoes.Count > 0; i++)
+            {                             
+                if (sortedDominoes[sortedDominoes.Count - 1][1] == dominoes[i].GetValues()[0])
+                {
+                    sortedDominoes.Add(dominoes[i].GetValues());
+                    dominoes.RemoveAt(i);
+                    i = -1;
+                }
+            }
+            return sortedDominoes;
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
+        }
+    }
+}
