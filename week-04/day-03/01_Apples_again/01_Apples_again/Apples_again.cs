7d1b326db361225659946ae72a34d2388768b7d5
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-27 13:01:26 +0200

    Apples - done

---
+++ week-04/day-03/01_Apples_again/01_Apples_again/Apples_again.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Apples_again
+{
+    public class Apples
+    {
+        public string GetApples()
+        {
+            return "apples";
+        }
+    }
+}
---
+++ week-04/day-03/01_Apples_again/ClassLibrary1/ApplesTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Apples_again;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class AppleTest
+    {
+        [Test]
+        public void TestGetApples()
+        {
+            var apples = new Apples();
+            string expected = "apples";
+            string output = apples.GetApples();
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
