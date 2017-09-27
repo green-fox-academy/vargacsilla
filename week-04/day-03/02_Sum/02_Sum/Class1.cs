0a390628ab18eb3710841d96e9bb1a30c37b09d0
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-27 13:54:44 +0200

    Sum - done

---
+++ week-04/day-03/02_Sum/02_Sum/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Sum
+{
+    public class MyClass
+    {
+        public int SumList(List<int> intList)
+        {
+            if (intList == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return intList.Sum();
+            }
+        }
+    }
+}
---
+++ week-04/day-03/02_Sum/TestSum/Class1.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02_Sum;
+
+namespace TestSum
+{
+    [TestFixture]
+    public class Class1
+    {
+        MyClass myClass = new MyClass();
+
+        [Test]
+        public void TestSumList_WhenEmpty_Returns0()
+        {
+            List<int> input = new List<int>() { };
+            int output = myClass.SumList(input);
+            int expected = 0;
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TestSumList_WhenOneElement_ReturnsElement()
+        {
+            List<int> input = new List<int>() { 5 };
+            int output = myClass.SumList(input);
+            int expected = 5;
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void TestSumList_WhenMultipleElements_ReturnsSum()
+        {
+            List<int> input = new List<int>() { 5, 5, 3 };
+            int output = myClass.SumList(input);
+            int expected = 13;
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TestSumList_WhenNull_ReturnsError()
+        {
+            List<int> input = null;
+            int output = myClass.SumList(input);
+            int expected = 0;S
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
