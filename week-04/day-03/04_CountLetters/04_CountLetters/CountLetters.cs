ccddbfebe3a5e28f60f9b7ab89d22c5dd540d59c
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-27 17:13:58 +0200

    Count Letters - done

---
+++ week-04/day-03/04_CountLetters/04_CountLetters/CountLetters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CountLetters
+{
+    public class CountLetters
+    {
+        public Dictionary<char, int> GetLettersCount(string inputString)
+        {
+            inputString = inputString.ToLower();
+            var letterDict = new Dictionary<char, int>();
+
+            if (String.IsNullOrEmpty(inputString))
+            {
+                return letterDict = null;
+            }
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (letterDict.ContainsKey(inputString[i]))
+                {
+                    letterDict[inputString[i]]++;
+                }
+                else
+                {
+                    letterDict.Add(inputString[i], 1);
+                }
+            }
+            return letterDict;
+        }
+    }
+}
---
+++ week-04/day-03/04_CountLetters/CountLettersTest/CountLettersClassTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _04_CountLetters;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CountLettersClassTest
+    {
+        CountLetters myCountLetter = new CountLetters();
+
+        [Test]
+        public void TestCountLetters_WhenEmpty_ReturnNull()
+        {
+            Dictionary<char, int> expected = null;
+            var input = "";
+
+            Assert.AreEqual(expected, myCountLetter.GetLettersCount(input));
+        }
+
+        [Test]
+        public void TestCountLetters_WhenContainsSame_ReturnDict()
+        {
+            //Dictionary<char, int> expected = new Dictionary<char, int>();
+            string input = "aba bba";
+            Dictionary<char, int> myDict = myCountLetter.GetLettersCount(input);
+            int output = myDict['b'];
+
+            int expected = 3;
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TestCountLetters_WhenUpperLower_ReturnDict()
+        {
+            //Dictionary<char, int> expected = new Dictionary<char, int>();
+            string input = "aBa bba";
+            Dictionary<char, int> myDict = myCountLetter.GetLettersCount(input);
+            int output = myDict['b'];
+
+            int expected = 3;
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
