845545f6cd070dd46028c635790c25a59f7e0a27
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-05 16:25:40 +0200

    feat(Carrier) - Carrier class initiated & AddAirCraft() added

---
+++ week-04/day-02/AirCraft/AirCraft/Carrier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirCraft
+{
+    class Carrier
+    {
+        List<Plane> planeList;
+        int healthPoints;
+        int ammoStore;
+        int currentAmmo = 0;
+        int aircraftCount;
+
+        public Carrier(int ammoStore, int healthPoints)
+        {
+            planeList = new List<Plane>();
+            this.ammoStore = ammoStore;
+            this.healthPoints = healthPoints;
+        }
+
+        public void AddAircraft(string type)
+        {
+            if (type == "F16")
+            {
+                planeList.Add(new F16());
+            }
+            if (type == "F35")
+            {
+                planeList.Add(new F35());
+            }
+        }
+    }
+}
--- week-04/day-02/AirCraft/AirCraft/Program.cs
+++ week-04/day-02/AirCraft/AirCraft/Program.cs
@@ -10,19 +10,11 @@ namespace AirCraft
     {
         static void Main(string[] args)
         {
-            var plane1 = new F16();
-            var plane2 = new F35();
-            Console.WriteLine(plane1.GetStatus());
-            Console.WriteLine("expected: 4, ");
-            Console.Write(plane1.Refill(12));
-            Console.WriteLine("excpected: 2, ");
-            Console.Write(plane1.Refill(2));
-            Console.WriteLine(plane1.GetStatus());
+            var myCarrier = new Carrier(150, 500);
+            myCarrier.AddAircraft("F16");
+            myCarrier.AddAircraft("F16");
+            myCarrier.AddAircraft("F35");
 
-            Console.WriteLine(plane2.GetStatus());
-            Console.WriteLine("expected: 0, ");
-            Console.Write(plane2.Refill(9));
-            Console.WriteLine(plane2.GetStatus());
             Console.Read();
         }
     }
