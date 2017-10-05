5b25d8656ec53fddc389b40649b67d9fd3fb0a98
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-05 13:58:04 +0200

    basic architecture done

---
+++ week-04/day-02/AirCraft/AirCraft/Plane.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirCraft
+{
+    class Plane
+    {
+        int maxAmmo;
+        int baseDamage;
+        int currentAmmo;
+
+        public Plane()
+        {
+            maxAmmo = 10;
+            baseDamage = 40;
+            currentAmmo = 0;
+        }
+
+        public void Fight()
+        {
+
+        }
+
+        public void Refill()
+        {
+
+        }
+
+        public string GetPlaneType()
+        {
+            return "";
+        }
+
+        public string Getstatus()
+        {
+            return "";
+        }
+    }
+}
