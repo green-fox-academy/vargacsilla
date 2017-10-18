23ee2c131f4f5ba25f9f7d2c7ba64dd19ab33086
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-18 16:32:37 +0200

    Exercise09 - half done, not sure if LINQ

---
+++ week-06/day-03/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myCharArray = { 'c', 's', 'i', 'L', 'l', 'A' };
+
+            var myString = string.Join("", myCharArray.Select(x => x));
+
+            Console.WriteLine(myString);
+            Console.Read();
+        }
+    }
+}
