ba41fc3d305fb3316fdb0eefa0a9dedad0964c1e
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-18 17:36:21 +0200

    Exercise07 done

---
+++ week-06/day-03/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var aICities = from city in cities
+                           where city.First() == 'A' && city.Last() == 'I'
+                           select city;
+
+            foreach (var item in aICities)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
