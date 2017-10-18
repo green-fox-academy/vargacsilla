d21bc169f94ff0629eca699a618d2e73f6ceed6d
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-18 17:31:45 +0200

    Exercise10 done

---
+++ week-06/day-03/Exercise10/Exercise10/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Fox
+    {
+        public string Name { get; protected set; }
+        public string Type { get; protected set; }
+        public ConsoleColor Color { get; protected set; }
+
+        public Fox(string name, string type, ConsoleColor color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
+    }
+}
---
+++ week-06/day-03/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foxList = new List<Fox>();
+            foxList.Add(new Fox("Rudi", "pallida", ConsoleColor.Green));
+            foxList.Add(new Fox("Vuk", "forest", ConsoleColor.Green));
+            foxList.Add(new Fox("Vori", "desert", ConsoleColor.Red));
+            foxList.Add(new Fox("Mazli", "arctic", ConsoleColor.White));
+            foxList.Add(new Fox("Csicsi", "pallida", ConsoleColor.Green));
+
+            //var greenFoxes = from fox in foxList
+            //                 where fox.Color == ConsoleColor.Green
+            //                 select fox;
+
+            var greenFoxes = foxList.Where(fox => fox.Color == ConsoleColor.Green).Select(fox => fox).ToList();
+
+            foreach (var item in greenFoxes)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var greenPallidaFoxes = (foxList.Where(fox => (fox.Color == ConsoleColor.Green) && (fox.Type == "pallida"))).Select(fox => fox).ToList();
+
+            Console.WriteLine();
+            foreach (var item in greenPallidaFoxes)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.Read();
+        }
+    }
+}
