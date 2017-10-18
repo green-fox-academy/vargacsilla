8e2738b19135545a57b652e912d0bf06807b84b7
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-18 16:49:50 +0200

    Exercise8 done

---
+++ week-06/day-03/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "vArga CSiLLa";
+
+            //var upperChars = from chars in testString
+            //                 where Char.IsUpper(chars)
+            //                 select chars;
+
+            var upperChars = testString.Where(x => Char.IsUpper(x)).Select(x => x);
+
+            foreach (var item in upperChars)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
