39e4580540fa30c388869175bd240f17bd066d75
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-18 15:17:06 +0200

    Exercise06 done - not much idea

---
+++ week-06/day-03/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "VaArga csillA";
+
+            var charCount = from character in testString.ToLower()
+                            group character by character into uniqueCharacters
+                            select new { uniqueCharacters.Key, Value = uniqueCharacters.Count() };
+
+            //var charCount = testString.ToLower().GroupBy(letter => letter).ToDictionary(letter => letter.Key, letter => letter.Count()).OrderBy(letter => letter.Key);
+
+            foreach (var item in charCount)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+            Console.Read();
+        }
+    }
+}
