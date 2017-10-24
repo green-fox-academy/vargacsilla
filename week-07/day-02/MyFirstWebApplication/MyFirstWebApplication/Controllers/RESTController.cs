a041fb12a6086f0a37e20f0dbe9d8b4e02de561f
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-24 14:15:34 +0200

    feat(RESTController) - greets input name

---
+++ week-07/day-02/MyFirstWebApplication/MyFirstWebApplication/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstWebApplication.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting(string name)
+        {
+            return new JsonResult(new Greeting { Id = 1, Content = $"hello, {name}!" });
+        }
+    }
+}
---
+++ week-07/day-02/MyFirstWebApplication/MyFirstWebApplication/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApplication.Models
+{
+    public class Greeting
+    {
+        public long Id;
+        public string Content;
+    }
+}
