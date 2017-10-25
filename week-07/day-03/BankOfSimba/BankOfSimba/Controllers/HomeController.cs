8967d108c0a445e1ab6f80aa3bef3e4dc2c2baf9
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-25 13:16:12 +0200

    Bank of Simba - part 1

---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [Route ("simba")]
+        public IActionResult GetAccount()
+        {
+            return View(new BankAccount("Simba", 2000, AnimalType.Lion));
+        }
+    }
+}
---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { set; get; }
+        public int Balance { set; get; }
+        public AnimalType AnimalType { set; get; }
+
+        public BankAccount(string name, int balance, AnimalType animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+    }
+}
