f0312d884c018b3f4a4ed308a5ccec54525cbce7
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-26 13:47:02 +0200

    ClickCounter done

---
+++ week-07/day-04/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Counter.Models;
+
+namespace Counter.Controllers
+{
+    public class HomeController : Controller
+    {
+        ClickCounter counter;
+
+        public HomeController(ClickCounter counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet]
+        [Route ("index")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost]
+        [Route("index")]
+        public IActionResult Increase()
+        {
+            counter.Raise();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-07/day-04/Counter/Counter/Models/ClickCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class ClickCounter
+    {
+        public int ClickCount { get; set; }
+
+        public void Raise()
+        {
+            ClickCount++;
+        }
+    }
+}
