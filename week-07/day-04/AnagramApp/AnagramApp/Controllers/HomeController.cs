8aa4989660af820013bb6f22e38a95bd9159e916
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-26 18:03:12 +0200

    AnagramApp done

---
+++ week-07/day-04/AnagramApp/AnagramApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AnagramApp.Models;
+
+namespace AnagramApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Anagram anagram;
+
+        public HomeController(Anagram anagram)
+        {
+            this.anagram = anagram;
+        }
+
+        [HttpGet]
+        [Route ("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("isAnagram")]
+        public IActionResult IsAnagram(string word1, string word2)
+        {
+            anagram.Word1 = word1;
+            anagram.Word2 = word2;
+
+            return RedirectToAction("Result");
+        }
+
+        [HttpGet]
+        [Route("isAnagram")]
+        public IActionResult Result()
+        {
+            return View(anagram);
+        }
+
+    }
+}
---
+++ week-07/day-04/AnagramApp/AnagramApp/Models/Anagram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnagramApp.Models
+{
+    public class Anagram
+    {
+        public string Word1 { get; set; }
+        public string Word2 { get; set; }
+
+        public bool IsAnagram()
+        {
+            char[] word1CharArray = Word1.ToLower().ToCharArray();
+            char[] word2CharArray = Word2.ToLower().ToCharArray();
+
+            Array.Sort(word1CharArray);
+            Array.Sort(word2CharArray);
+
+            return new string(word1CharArray) == new string(word2CharArray);
+        }
+    }
+}
