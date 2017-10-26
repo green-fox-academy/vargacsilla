11412e11e273a15ab047368c3a0bf9b6bc68ece2
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-26 16:51:46 +0200

    GreetApp done

---
+++ week-07/day-04/GreetApp/GreetApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using GreetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        User user;
+
+        public HomeController(User user)
+        {
+            this.user = user;
+        }
+
+        [HttpGet]
+        [Route ("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route ("greet")]
+        public IActionResult GiveName(string name)
+        {
+            user.Name = name;
+            return RedirectToAction("Greet");
+        }
+
+        [HttpGet]
+        [Route ("greet")]
+        public IActionResult Greet()
+        {
+            return View(user);
+        }
+    }
+}
---
+++ week-07/day-04/GreetApp/GreetApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Greet()
+        {
+            return $"Hello, dear {Name}!";
+        }
+    }
+}
