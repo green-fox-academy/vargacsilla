5856a933bb2d51880b9d6d92ed8c9d7ddf5c2f41
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-31 13:37:30 +0100

    class(App): new Todo class added

---
+++ week-08/day-02/TodoApp/TodoApp/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        bool IsUrgent { get; set; } = false;
+        bool IsDone { get; set; } = false;
+    }
+}
