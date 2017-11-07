ae7f21753d67262eada7f02cd80c77b701a43f2b
Csilla Varga <vargacsillamagda@gmail.com>
2017-11-07 14:00:12 +0100

    RestBackend - doubling, greeter, appenda added

---
+++ week-09/day-02/RestBackend/RestBackend/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet]
+        [Route("/doubling")]
+        public IActionResult Doubling(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            return Json(new { received = input, result = input*2 });
+        }
+
+        [HttpGet]
+        [Route("/greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            return Json(new { Welcome_message = $"Oh, hi there {name}, my dear {title}!"});
+        }
+
+        [HttpGet]
+        [Route("/appenda/{appendable}")]
+        public IActionResult AppendA(string appendable)
+        {
+            if (appendable == null)
+            {
+                return NotFound();
+            }
+            return Json(new { appended = appendable + "a" });
+        }
+
+    }
+}
