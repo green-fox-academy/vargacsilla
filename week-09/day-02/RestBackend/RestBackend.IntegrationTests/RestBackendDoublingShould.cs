3803e81eb2e9318b9546da344147ce0c9add784b
Csilla Varga <vargacsillamagda@gmail.com>
2017-11-08 14:29:59 +0100

    RestBackend - DoublingTests added

---
+++ week-09/day-02/RestBackend/RestBackend.IntegrationTests/RestBackendDoublingShould.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net.Http;
+using Xunit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace RestBackend.IntegrationTests
+{
+    public class RestBackendDoublingShould
+    {
+        private TestServer Server;
+        private HttpClient Client;
+
+        public RestBackendDoublingShould()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await Client.GetAsync("doubling");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReturnErrorMessage()
+        {
+            var response = await Client.GetAsync("doubling");
+
+            Assert.Equal("{\"error\":\"Please provide an input!\"}", await response.Content.ReadAsStringAsync());
+        }
+
+        [Fact]
+        public async Task WhenNumberEnteredReturnResult()
+        {
+            var response = await Client.GetAsync("doubling?input=55");
+            var resultMessage = await response.Content.ReadAsStringAsync();
+
+            Assert.True(resultMessage.ToString().Contains("result"));
+        }
+    }
+}
---
+++ week-09/day-02/RestBackend/RestBackend.IntegrationTests/RestBackendGreeterShould.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestBackend.IntegrationTests
+{
+    class RestBackendGreeterShould
+    {
+        private TestServer Server;
+        private HttpClient Client;
+
+        public RestBackendGreeterShould()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await Client.GetAsync("greeter");
+            var responseMessage = await response.Content.ReadAsStringAsync();
+
+            Assert.True(responseMessage.Contains("error"));
+        }
+    }
+}
