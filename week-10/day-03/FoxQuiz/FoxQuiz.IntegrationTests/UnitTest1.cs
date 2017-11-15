086dcedfbde720433a462fa8073c8e2b19d712e6
Csilla Varga <vargacsillamagda@gmail.com>
2017-11-15 15:22:07 +0100

    FoxQuiz - Initial commit

---
+++ week-10/day-03/FoxQuiz/FoxQuiz.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FoxQuiz.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
